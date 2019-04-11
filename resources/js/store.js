8629c1912b3f06ac6e76e92bb8d249ff611d51a4
Ing Geronimo Diaz <gediaz@uno.com.do>
2019-04-11 12:12:10 -0400

    Completed

---
+++ resources/js/store.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    token: null,
+    usuario: null
+  },
+  mutations: {
+    setUsuario(state, usuario) {
+      state.usuario = usuario
+    }
+  },
+  actions: {
+    guardarUser({ commit }, user) {
+      commit("setUsuario", user)
+      localStorage.setItem("user", user)
+    },
+    autoLogin({ commit }) {
+      let user = localStorage.getItem("user")
+      if (user) {
+        commit("setUsuario", user)
+        router.push({ name: 'home' })
+      } else {
+        router.push({ name: 'login' })
+      }
+    },
+    salir({ commit }) {
+      commit("setUsuario", null)
+      localStorage.removeItem("user")
+      router.push({ name: 'login' })
+    }
+  }
+})
